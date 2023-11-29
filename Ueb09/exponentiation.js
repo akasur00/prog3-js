b9fd59f34b9badc4deae4dc24dde2aaa7b3dd550
it.willihuebner@protonmail.com <it.willihuebner@protonmail.com>
2023-11-29 16:43:37 +0100

    finalized exponentiation.js

--- Ueb09/exponentiation.js
+++ Ueb09/exponentiation.js
@@ -1,4 +1,21 @@
 
+const decrement_base = 0.01;
+const decrement_exponent = 0.01;
 function exponentiation(){
+    let base = 1.0;
+    let exponent = 1.0;
 
+    //loop with decrementing base
+    for (let i = 0; i < 101; i++) {
+        console.log(`base: ${base}\texponent: ${exponent}\n`, base ** exponent);
+        base -= decrement_base;
+        exponent -= decrement_exponent;
+    }
+
+    console.log()
+
+    console.log("Exponentiation results:",
+    "\n0^1:\t", 0 ** 1,
+    "\n1^0:\t", 1 ** 0,
+    "\n0^0:\t", 0 ** 0);
 }
